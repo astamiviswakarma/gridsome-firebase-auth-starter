b554394006e1e5f27492b31ce08e8e299dff98d4
Nitin Sharma <nitin@meddo.in>
2022-02-08 23:49:15 +0530

    Added GraphQL codegen

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,7 +10,19 @@ module.exports = {
     {
       use: '~/plugins/gridsome-plugin-firebase-auth',
       options: { //should be copied from `const firebaseConfig = "object"` line
-
+        apiKey: "AIzaSyDXQudbi3R0bLRsTUGorp56x4wLsy0neKA",
+        authDomain: "arogyadham-shipperstack.firebaseapp.com",
+        projectId: "arogyadham-shipperstack",
+        storageBucket: "arogyadham-shipperstack.appspot.com",
+        messagingSenderId: "966530457473",
+        appId: "1:966530457473:web:d8af31b2942f8f31d29280",
+        measurementId: "G-DGVFYY8KE9"
+      }
+    },
+    {
+      use: '~/plugins/gridsome-plugin-graphql-codegen',
+      options: {
+        configPath: './codegen.yml',
       }
     }
   ]
